10cdc201b8a96b5926feac9d1f2d0054d519af4f
prithviman Bhattacharjee <prithvimanb@gmail.com>
2026-03-19 10:49:27 +0530

    fixed  base url  for live api in  render

--- frontend-Admin/src/api/client.js
+++ frontend-Admin/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // We created the backend to run on port 3000
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://farmersapp-333z.onrender.com/api';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
